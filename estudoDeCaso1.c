ff30c68d52c62c50893d863511b875614b816949
Phenrique1628 <161074814+Phenrique1628@users.noreply.github.com>
2026-06-08 21:44:32 -0400

    Change function return types to void and fix types

--- estudoDeCaso1.c
+++ estudoDeCaso1.c
@@ -3,16 +3,16 @@
 int id[5]={10, 5, 78, 504, 90};
 float saldo[5]={200.00, 00.00, 1621.00 , 100.00, 50.00 };
 
-float deposito(int conta, float valor){
+void deposito(int conta, float valor){
     saldo[conta] += valor;
     printf("\nO saldo da conta será de: %f\n", saldo[conta]);
 }
-float saque(int conta, float valor){
+void saque(int conta, float valor){
     saldo[conta] -= valor;
     printf("\nO saldo da conta será de: %f\n", saldo[conta]);
 }
-float ativo(){
-    float soma;
+void ativo(){
+    float soma = 0;
     for(int i=0;i<5;i++){
         soma += saldo[i];
     }
@@ -28,8 +28,8 @@ int encontrar(int conta){
     printf("\nConta não encontrada!\n");
     return -1;
 }
-int verificar(int conta, int valor){
-    if(saldo[encontrar(conta) < valor])
+int verificar(int conta, float valor){
+    if(saldo[encontrar(conta)] >= valor)
         return 1;
     else 
         printf("\nSaldo insulficiente!\n");
@@ -37,7 +37,8 @@ int verificar(int conta, int valor){
 }
 int main()
 {
-    int operacao, conta, valor;
+    int operacao, conta;
+    float valor;
     
     do{
         printf("\n1 – Efetuar depósito\n2 – Efetuar saque\n3 – Consultar o ativo bancário\n4 – Finalizar o programa \nDigite a operação desejada: ");
@@ -55,7 +56,7 @@ int main()
             printf("Digite a conta bancária da operação: ");
             scanf("%d", &conta);
             printf("Digite o valor da operação: ");
-            scanf("%d", &valor);
+            scanf("%f", &valor);
             if(encontrar(conta) != -1){
                 if(verificar(conta, valor) == 1)
                     saque(encontrar(conta), valor);
